b0b86df3282e4c0eb20cfc3a1a65c359c36623c0
sujal mallick <mallicksujal626@gmail.com>
2025-05-29 10:42:10 +0530

    remove comments

--- js_series_2/09_array.js
+++ js_series_2/09_array.js
@@ -2,29 +2,29 @@
 // // These values (called elements) can be of any type, including numbers, strings, objects, other arrays, and more.
 
 const myArr=[0,1,2,3,4,5]//declaration
-// const myArr2=new Array(1,2,3,4)//declared using new keyword
-// console.log(myArr[0]);//accessing using indexing
+const myArr2=new Array(1,2,3,4)//declared using new keyword
+console.log(myArr[0]);//accessing using indexing
 
 
-// //Array Method
+//Array Method
 
-// myArr.push(6);//add an element to the end of an array
-// console.log(myArr);
+myArr.push(6);//add an element to the end of an array
+console.log(myArr);
 
-// myArr.pop()//remove the last element
-// console.log(myArr);
+myArr.pop()//remove the last element
+console.log(myArr);
 
-// myArr.unshift(8)//Add element to the start of array
-// console.log(myArr);
+myArr.unshift(8)//Add element to the start of array
+console.log(myArr);
 
-// myArr.shift()//remove the first element
-// console.log(myArr);
+myArr.shift()//remove the first element
+console.log(myArr);
 
-// console.log(myArr.includes(6));//return true of false
-// console.log(myArr.indexOf(2));//return the index of a value
+console.log(myArr.includes(6));//return true of false
+console.log(myArr.indexOf(2));//return the index of a value
 
-// const newArr=myArr.join()//add all elements with a seperator in a string form
-// console.log(newArr);
+const newArr=myArr.join()//add all elements with a seperator in a string form
+console.log(newArr);
 
 console.log("A ",myArr);
 
